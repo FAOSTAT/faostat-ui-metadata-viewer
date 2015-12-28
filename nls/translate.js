fb44a99c61844f3a678992c5f31126e78ab92fb5
Kalimaha <guido.barbaglia@gmail.com>
2015-12-28 10:53:36 +0100

    Courtesy message for empty metadata.

--- nls/translate.js
+++ nls/translate.js
@@ -1,10 +1,12 @@
+/*global define*/
 define({
     root : {
         language: 'Language',
         error: 'FENIX UI Metadata Viewer',
         identification: 'IDENTIFICATION',
         courtesy: 'No data available for the provided resource ID.',
-        export_pdf_label: 'Export to PDF'
+        export_pdf_label: 'Export to PDF',
+        no_metadata_available: 'There is no metadata available for this domain yet.<br>We apologize for any inconvenience.'
     },
     fr : true,
     es : true
