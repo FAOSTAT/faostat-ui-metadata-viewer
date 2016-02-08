5027d76359066dd7f93e6c163d7bf78079058146
simonemurzilli <simone.murzilli@fao.org>
2016-02-08 18:20:55 +0100

    new metadata viewer

---
+++ config/Config.js
@@ -0,0 +1,12 @@
+/*global define*/
+define(function () {
+
+        'use strict';
+
+        return {
+
+            "URL_METADATA_MODEL": 'http://faostat3.fao.org/mdfaostat/getmd/',
+            "URL_METADATA_DOMAIN": 'http://faostat3.fao.org/mdfaostat/getdomain/',
+
+        };
+    });
