f2d266cd5b6112491817fdac0a5345585c24ff3c
simonemurzilli <simone.murzilli@fao.org>
2016-02-09 10:27:12 +0100

    added modal

--- nls/translate.js
+++ nls/translate.js
@@ -2,6 +2,7 @@
 define({
     root: {
         metadata: 'Metadata',
+        close: 'Close',
         courtesy: 'No data available for the provided resource ID.',
         export_pdf_label: 'Export to PDF',
         no_metadata_available: 'There is no metadata available for this domain yet.<br>We apologize for any inconvenience.'
